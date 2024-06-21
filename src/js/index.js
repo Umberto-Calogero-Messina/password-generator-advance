b45c1edc7eb6aa016494ee0fdd2bbb70e7871d32
Ucmessina <ucmessina@alumnostrazos.net>
2024-06-21 14:03:56 +0200

    +FirstCommit

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,38 @@
-// El styles lo importamos aquí, ya se carga después al compilar todo
 import '../scss/styles.scss';
+
+const textLengthElement = document.getElementById('text-length');
+const buttonElement = document.getElementById('generate__password');
+const rangeElement = document.getElementById('range');
+const passwordElement = document.getElementById('password');
+
+const lowercase = 'abcdefghijklmnñopqrstuvwxyz';
+const uppercase = 'ABCDEFGHIJKLMNÑOPQRSTUVWXYZ';
+const numbers = '1234567890';
+const symbols = '+-.,!"·$%&/()=?{}';
+
+let passwordLength = 16;
+
+const generateRandomNumber = () => {
+  return Math.floor(Math.random() * allowedChar.length);
+};
+
+const setLengthPassword = ev => {
+  passwordLength = ev.target.value;
+  textLengthElement.textContent = passwordLength;
+};
+
+const generatePassword = () => {
+  let finalPassword = '';
+  for (let i = 0; i < passwordLength; i++) {
+    const randomNumber = generateRandomNumber();
+    finalPassword += allowedChar[randomNumber];
+  }
+  return finalPassword;
+};
+
+const printPasswordValue = () => {
+  passwordElement.value = generatePassword();
+};
+
+rangeElement.addEventListener('input', setLengthPassword);
+buttonElement.addEventListener('click', printPasswordValue);
