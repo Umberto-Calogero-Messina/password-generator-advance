1431ac3ecd980b40397037c525816f97ba5e2a19
Genezis <ucmessina@alumnostrazos.net>
2024-06-24 21:51:00 +0200

    +FinalWork

--- src/js/index.js
+++ src/js/index.js
@@ -1,36 +1,11 @@
 import '../scss/styles.scss';
 
-/*
-## Normas del ejercicio
-
-
-
-- En la parte de STRENGTH tenemos 4 valores posibles:
-
-  - TOO SHORT: Este valor aparecerá siempre que el password tenga 5 o menos caracteres.
-  - WEAK:Este valor aparecerá si el range es superior a 5 y menor de 10.
-  - MEDIUM: Este valor aparecerá si el range es superior a 10 y menor a 15.
-  - STRENGTH: Este valor aparecerá si el range es superior a 15.
-
-- El botón SÓLO se podrá pulsar cuando la longitud sea mayor de 5
-
-## Pistas y pasos a seguir
-
-- Crear las 2 cajas iniciales
-- Poner los textos
-- Sincronizar el length del nuevo password con el input range
-- Conseguir que se genere un string de esa longitud al pulsar el botón y se escriba en el primer input.
-
-## Principios Importantes para la creación de aplicaciones.
-
-- Las funciones deben tener UNA ÚNICA responsabilidad, Es mejor tener 5 funciones con 2 líneas cada una que tener una sola función que se encargue de 5 tareas distintas.
-*/
-
 const textLengthElement = document.getElementById('text-length');
 const buttonElement = document.getElementById('generate__password');
 const rangeElement = document.getElementById('range');
 const passwordElement = document.getElementById('password');
 const optionsElement = document.getElementById('options');
+const strengthElement = document.getElementById('strength__password');
 
 let allowedChar = '';
 
@@ -46,6 +21,16 @@ let passwordLength = 16;
 const setLengthPassword = ev => {
   passwordLength = ev.target.value;
   textLengthElement.textContent = passwordLength;
+
+  if (passwordLength <= 5) {
+    strengthElement.textContent = `TOO SHORT`;
+  } else if (passwordLength > 5 && passwordLength < 10) {
+    strengthElement.textContent = `WEAK`;
+  } else if (passwordLength >= 10 && passwordLength < 15) {
+    strengthElement.textContent = `MEDIUM`;
+  } else if (passwordLength >= 15) {
+    strengthElement.textContent = `STRENGTH`;
+  }
 };
 
 const setInputValue = event => {
@@ -56,11 +41,12 @@ const setInputValue = event => {
 };
 
 const calcPasswordOptions = () => {
-  allowedCharacters = '';
+  allowedChar = '';
   const setCheckbox = document.querySelectorAll('input:checked');
   setCheckbox.forEach(checkbox => {
-    allowedCharacters += passwordSettings[checkbox.id];
+    allowedChar += passwordSettings[checkbox.id];
   });
+  console.log(allowedChar);
 };
 
 const generateRandomNumber = () => {
@@ -77,6 +63,10 @@ const generatePassword = () => {
   return finalPassword;
 };
 
+const printPasswordValue = () => {
+  passwordElement.value = generatePassword();
+};
+
 rangeElement.addEventListener('input', setLengthPassword);
 optionsElement.addEventListener('click', setInputValue);
-buttonElement.addEventListener('click', generatePassword);
+buttonElement.addEventListener('click', printPasswordValue);
